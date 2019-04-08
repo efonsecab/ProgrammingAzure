f059847fdb64546177c44ac2ad1a3b3a22cc2c2b
Eduardo Fonseca <efonsecab@gmail.com>
2019-04-08 12:10:04 -0600

    Adding test project

---
+++ ProgrammingAzureSln/FileStorageTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileStorageTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
